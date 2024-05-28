6dd87a0b2c3c938a06b4105dcf718d9ac801a4bc
Sebastian Baltes <research@sbaltes.com>
2024-05-28 17:24:50 +0200

    Add OpenAPI/Swagger UI

--- api/src/main/java/de/unibayreuth/se/teaching/list/api/controller/ListController.java
+++ api/src/main/java/de/unibayreuth/se/teaching/list/api/controller/ListController.java
@@ -3,6 +3,11 @@ package de.unibayreuth.se.teaching.list.api.controller;
 import de.unibayreuth.se.teaching.list.api.dto.ListElementDto;
 import de.unibayreuth.se.teaching.list.api.mapper.ListElementDtoMapper;
 import de.unibayreuth.se.teaching.list.business.ports.ListService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,12 +17,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ListAPI",
+                version = "0.0.1"
+        )
+)
 @Controller
 @RequiredArgsConstructor
 public class ListController {
     private final ListService listService;
     private final ListElementDtoMapper listElementDtoMapper;
 
+    @Operation(
+            summary = "Get all list elements.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json"),
+                            description = "All list elements as a JSON array."
+                    )
+            }
+    )
     @GetMapping(value = "/list")
     public ResponseEntity<List<ListElementDto>> getList() {
         return ResponseEntity.ok(listService.get().stream()
@@ -28,6 +49,16 @@ public class ListController {
         );
     }
 
+    @Operation(
+            summary = "Appends elements to the list.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json"),
+                            description = "The list including the appended list elements as a JSON array."
+                    )
+            }
+    )
     @PostMapping(value = "/list")
     public ResponseEntity<List<ListElementDto>> appendList(@RequestBody List<ListElementDto> elements) {
         listService.append(elements.stream()
